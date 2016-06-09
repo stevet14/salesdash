75a46481dac4fc75a42194c54972b12a2d50b1c9
stevet14 <steve.taplin@gmail.com>
2016-06-09 22:09:04 +0100

    Added Navbar and Footer

--- app/components/App.js
+++ app/components/App.js
@@ -2,15 +2,19 @@
  * Created by stevet on 07/06/2016.
  */
 import React from 'react';
+import Navbar from './Navbar';
+import Footer from './Footer';
 
 class App extends React.Component {
     render() {
         return (
             <div>
+                <Navbar history={this.props.history} />
                 {this.props.children}
+                <Footer />
             </div>
-);
-}
+        );
+    }
 }
 
 export default App;
--- app/routes.js
+++ app/routes.js
@@ -9,5 +9,5 @@ import Home from './components/Home';
 export default (
 <Route component={App}>
     <Route path='/' component={Home} />
-    </Route>
+</Route>
 );
